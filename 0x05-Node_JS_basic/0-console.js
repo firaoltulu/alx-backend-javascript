6f788daa4c89fa1475811b80d74ad3c04c60ff11
FiraoolTulu <117259416+firaoltulu@users.noreply.github.com>
2024-06-04 22:22:20 +0300

    my first commit

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,15 @@
+/**
+ * This Program - Logs a message to the console.
+ * @param {string} message - The message to log.
+ * @returns {void}
+ */
+
+const displayMessage = (message) => console.log(message);
+
+/**
+ * Exports the displayMessage function.
+ * @module displayMessage
+ * @function
+ */
+// Default module export.
+module.exports = displayMessage;
